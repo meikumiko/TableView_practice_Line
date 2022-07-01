d3a54df48ac27417a8e1fd33b06b43bac7892c6b
Hannah Chi <cremi468@gmail.com>
2022-07-02 01:23:52 +0800

    Initial Commit

---
+++ TableView_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_practice
+//
+//  Created by Hsiao-Han Chi on 2022/7/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
